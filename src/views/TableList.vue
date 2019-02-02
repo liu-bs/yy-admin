55f3d6edf45d74b0a7fdaf5bd0412303b5de3fda
Lbs <805512979@qq.com>
2019-02-02 19:33:55 +0800

    基本完成
    Signed-off-by: Lbs <805512979@qq.com>

--- src/views/TableList.vue
+++ src/views/TableList.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="xfn-table-list">
+  <div class="yy-table-list">
     <el-breadcrumb>
       <el-breadcrumb-item to="/main">首页</el-breadcrumb-item>
       <el-breadcrumb-item>桌台管理</el-breadcrumb-item>
       <el-breadcrumb-item>桌台列表</el-breadcrumb-item>
     </el-breadcrumb>
     <br>
+    
     <el-row>
-      <el-col v-for="(t,i) in tableList" :xs="12" :sm="8" :md="6" :lg="4" xl="2">
-        <xfn-table :data="t"></xfn-table>
+      <el-col v-for="(t,i) in tableList" :key="i" :xs="12" :sm="8" :md="6" :lg="4" :xl="2">
+        <yy-table :data="t"></yy-table>
       </el-col>
     </el-row>  
   </div>
@@ -22,8 +23,17 @@ export default {
       tableList: []
     }
   },
+  mounted(){
+    //加载桌台列表
+    var url = this.$store.state.globalSettings.apiUrl + '/admin/table';
+    this.$axios.get(url).then(({data})=>{
+      this.tableList = data;
+    }).catch((err)=>{
+      console.log(err);
+    })
+  },
   components: {
-    'xfn-table': Table
+    'yy-table': Table
   }
 }
 </script>
